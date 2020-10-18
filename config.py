6e453bf5d7ec2a98a9e61615319586dbdca7f0a3
Adin Lukas DelaCruz <lukeisun57@gmail.com>
2020-10-18 14:36:36 -0400

    Updated config.py to make it cleaner to read

--- config.py
+++ config.py
@@ -1,4 +1,13 @@
-TOKEN = "" # add your own discord token, as well as server ID and channel ID
-SERVER = ""
-BOTCHANNEL =
-WELCOMEMSG = "!"
+TOKEN = "" # add your own discord token
+SERVER = "" # Server ID
+######################################################################################################################
+FLAGFORINDIVIDUALCHANNEL = 0  # set to 0 for separate channel postiing OR 1 for general live update in one channel.
+ # If you set it to 0 fill in below variables with your channel IDS
+CHANNELEN =
+CHANNELJP =
+CHANNELID =
+CHANNELST =
+ # If you set it to 1 fill in this variable (Uncomment it and commet above variables with #)
+ # BOTCHANNEL =
+######################################################################################################################
+WELCOMEMSG = "Kikkeriki!"
